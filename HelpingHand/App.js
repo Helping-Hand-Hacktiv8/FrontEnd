bd3ceba08d6824a739d6c23034e5e2db8e37adb3
damaramay <damaraharahap@gmail.com>
2023-09-25 13:06:02 +0700

    feat: home styling

--- HelpingHand/App.js
+++ HelpingHand/App.js
@@ -2,34 +2,30 @@ import { NavigationContainer } from "@react-navigation/native";
 import { Feather } from "@expo/vector-icons";
 import HomeStack from "./scr/routes/HomeStack";
 import ProfileStack from "./scr/routes/ProfileStack";
-import { Ionicons } from '@expo/vector-icons'; 
+import { Ionicons } from "@expo/vector-icons";
 import { FontAwesome5 } from "@expo/vector-icons";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import { StyleSheet, Text } from "react-native";
-import { useTheme } from 'react-native-paper';
+import { useTheme } from "react-native-paper";
 import MyActivityStack from "./scr/routes/MyActivityStack";
-import { Entypo } from '@expo/vector-icons'; 
-
-
-
+import { Entypo } from "@expo/vector-icons";
 
 export default function App() {
   const theme = useTheme();
-theme.colors.secondaryContainer = "transperent"
+  theme.colors.secondaryContainer = "transperent";
   const Tab = createMaterialBottomTabNavigator();
   return (
     <NavigationContainer>
       <Tab.Navigator
-        activeColor="#dc6c3c"d
+        activeColor="white"
         barStyle={{
-          backgroundColor: "white",
+          backgroundColor: "#DA622F",
           height: 70,
-          
         }}
         screenOptions={{
           headerShown: false,
           tabBarActiveTintColor: "#175d8c",
-          
+
         }}
       >
         <Tab.Screen
@@ -37,30 +33,22 @@ theme.colors.secondaryContainer = "transperent"
           component={HomeStack}
           options={{
             title: "Home",
-            tabBarIcon: ({ color }) => (
-              <Entypo name="home" size={24} color={color} />
-
-            ),
-            
+            tabBarIcon: ({ color }) => <Entypo name="home" size={24} color={color} />,
           }}
         />
         <Tab.Screen
           name="MyActivity"
           component={MyActivityStack}
           options={{
-            title:"My Activity",
-            tabBarIcon: ({ color }) => (
-              <Entypo name="clipboard" size={24} color={color} />
-            ),
+            title: "My Activity",
+            tabBarIcon: ({ color }) => <Entypo name="clipboard" size={24} color={color} />,
           }}
         />
         <Tab.Screen
           name="My Rewards"
           component={HomeStack}
           options={{
-            tabBarIcon: ({ color }) => (
-              <Ionicons name="ios-gift-sharp" size={24} color={color} />
-            ),
+            tabBarIcon: ({ color }) => <Ionicons name="ios-gift-sharp" size={24} color={color} />,
           }}
         />
         <Tab.Screen
@@ -68,20 +56,15 @@ theme.colors.secondaryContainer = "transperent"
           component={ProfileStack}
           options={{
             title: "My Profile",
-            tabBarIcon: ({ color }) => (
-              <FontAwesome5 name="user-alt" size={24} color={color} />
-            ),
+            tabBarIcon: ({ color }) => <FontAwesome5 name="user-alt" size={24} color={color} />,
           }}
         />
-         <Tab.Screen
+        <Tab.Screen
           name="Chat"
           component={HomeStack}
           options={{
             title: "Chat",
-            tabBarIcon: ({ color }) => (
-              <Entypo name="chat" size={24} color={color} />
-
-            ),
+            tabBarIcon: ({ color }) => <Entypo name="chat" size={24} color={color} />,
           }}
         />
       </Tab.Navigator>
--- HelpingHand/scr/screens/HomePage.js
+++ HelpingHand/scr/screens/HomePage.js
@@ -1,50 +1,138 @@
 import { useNavigation } from "@react-navigation/native";
-import { View, Text, ScrollView} from "react-native";
+import { View, Text, ScrollView, TextInput, TouchableOpacity, Image, ActivityIndicator, StyleSheet } from "react-native";
 import { Button, SearchBar } from "@rneui/base";
 import { useState } from "react";
 import CardComp from "../components/CardComp";
+import searchIcon from "../../assets/search.png"
 
-
-
-export default function Home({}) {
+export default function Home({ searchTerms, setSearchTerms, handleClick }) {
   const navigation = useNavigation();
   const [search, setSearch] = useState("");
+
+  const isLoading = true
   let isLogin = true;
   if (isLogin) {
     return (
-      <View style={{ backgroundColor: "white", flex:1 }}>
-        <SearchBar
-          round
-          searchIcon={{ size: 24 }}
-          placeholder="Search Activity.."
-          value={search}
-          containerStyle={{
-            backgroundColor: "transparent",
-            borderColor: "transparent",
-            marginTop:5
+      <View style={{ backgroundColor: "white", flex: 1 }}>
+        <Text
+          style={{
+            textAlign: "left",
+            fontWeight: "600",
+            marginBottom: 1,
+            fontSize: 25,
+            padding: 10,
           }}
-          inputContainerStyle={{
-            backgroundColor:'white',
-            elevation:5
-          }}
-        />
-        <ScrollView style={{flex:1}}>
-            <View style={{flex:1, marginVertical:15,alignItems:'center', justifyContent:'center'}}>
-        <Text style={{
-             textAlign: "left",
-             fontStyle: "italic",
-             fontWeight: "800",
-             fontSize: 25,
-             padding:10
-        }}>
-            Available Task
+        >
+          Welcome "User"
         </Text>
-            <CardComp/>
-            <CardComp/>
-            <CardComp/>
-            <CardComp/>
+        <Text
+          style={{
+            textAlign: "left",
+            fontWeight: "300",
+            fontSize: 14,
+            padding: 10,
+          }}
+        >
+          Which good deeds you want to do today?
+        </Text>
 
-            </View>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            marginTop: 20,
+            height: 50,
+            borderColor: "gray",
+            borderWidth: 1,
+            borderRadius: 16,
+            overflow: "hidden",
+          }}
+        >
+          <View
+            style={{
+              flex: 1,
+              backgroundColor: "white",
+              marginRight: 2,
+              borderRadius: 16,
+              flexDirection: "row",
+              alignItems: "center",
+              paddingHorizontal: 10,
+              height: "100%",
+            }}
+          >
+            <TextInput
+              style={{
+                flex: 1,
+                height: "100%",
+              }}
+              value={searchTerms}
+              onChangeText={(text) => setSearchTerms(text)}
+              placeholder="What kind of deeds you want to have?"
+            />
+          </View>
+
+          <TouchableOpacity
+            style={{
+              width: 50,
+              height: "100%",
+              backgroundColor: "#FF7754",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+            onPress={handleClick}
+            >
+            <Image
+            source={searchIcon}
+            resizeMode='contain'
+            style={{
+              width: "50%",
+              height: "50%",
+              tintColor: "#F3F4F8"
+            }}
+          />
+          </TouchableOpacity>
+        </View>
+
+        <View>
+        <TouchableOpacity
+        style={{
+          
+        }}
+        >
+
+        </TouchableOpacity>
+        </View>
+
+        <View style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          marginTop: 2
+        }}>
+
+        <Text style={{
+          fontSize: 20,
+          color: "#312651",
+        }}>Nearby Helps</Text>
+        <TouchableOpacity>
+          <Text style={{
+          fontSize: 20,
+          color: "#312651",
+        }}>Show all</Text>
+        </TouchableOpacity>
+      </View>
+
+        <ScrollView style={{ flex: 1 }}>
+          <View style={{ flex: 1, marginVertical: 15, alignItems: "center", justifyContent: "center", marginTop: 16 }}>
+          {isLoading ? (
+          <ActivityIndicator size="large" color={"#312651"} />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+          ) : (
+            data?.map((data) => <CardComp data={data} key={`nearby-data-${data.id}`} handleNavigate={() => {}} />)
+            )}
+            <CardComp />
+          </View>
         </ScrollView>
       </View>
     );
@@ -74,3 +162,7 @@ export default function Home({}) {
     );
   }
 }
+
+const styles = StyleSheet.create({
+
+})
