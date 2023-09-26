5ef668e1a05530489c7a3a83b37f5701b3e5c7ea
damaramay <damaraharahap@gmail.com>
2023-09-26 20:39:32 +0700

    docs: fixing minor bugs

--- HelpingHand/scr/screens/ProfilePage.js
+++ HelpingHand/scr/screens/ProfilePage.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { View, Text, Button, StyleSheet, Alert, ScrollView, Image, TouchableOpacity } from "react-native";
+import React, { useCallback, useEffect, useState } from "react";
+import { View, Text, Button, StyleSheet, Alert, ScrollView, Image, TouchableOpacity, SafeAreaView, ActivityIndicator } from "react-native";
 import * as SecureStore from "expo-secure-store";
 import { useDispatch, useSelector } from "react-redux";
-import { editUserToken } from "../store/actions/actionCreator";
+import { asyncFetchSingleUser, editUserToken } from "../store/actions/actionCreator";
+import { useFocusEffect } from "@react-navigation/native";
 
 export default function ProfilePage({ navigation }) {
   const dispatch = useDispatch();
@@ -12,6 +13,8 @@ export default function ProfilePage({ navigation }) {
 
   const [editingPassword, setEditingPassword] = useState(false);
   const [newPassword, setNewPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  let [userId, setUserId] = useState(0)
 
   const handleChat = () => {
     console.log("Chat button clicked");
@@ -24,77 +27,91 @@ export default function ProfilePage({ navigation }) {
     } catch (err) {
       console.log(err);
     }
-  };
+  }
 
+  useFocusEffect(
+    useCallback(() => {
+      async function getUser() {
+        let getId = await SecureStore.getItemAsync("user_id");
+        setUserId(getId)
+        dispatch(asyncFetchSingleUser(userId))
+          .then(() => {
+            setIsLoading(false)
+          })
+      }
 
+      getUser()
+    }, [userId])
+  )
 
-  console.log(user,"<<<<<<di profile"); 
-  
-  if (user) return (
+  if (isLoading) {
+    return (
+      <SafeAreaView style={{ flex: 1 }}>
+        <ActivityIndicator size="large" color={"#312651"} />
+      </SafeAreaView>
+    );
+  } else {
+    return (
+      <ScrollView>
+        <View style={styles.container}>
+          {/* untuk profile photo */}
+          <View style={{ flex: 1 }}>
+            <Image
+              source={{ uri: user.profileImg }}
+              style={{
+                width: 150,
+                height: 150,
+                paddingTop: 5,
+                marginBottom: 5,
+                marginTop: 10,
+                borderRadius: 100,
+                borderColor: "black",
+              }}
+            />
+          </View>
 
-    <ScrollView>
-      <View style={styles.container}>
-        {/* untuk profile photo */}
-        <View style={{flex: 1}}>
-          <Image
-            source={{uri: user.profileImg}}
-            style={{
-              width: 150,
-              height: 150,
-              paddingTop: 5,
-              marginBottom: 5,
-              marginTop: 10,
-              borderRadius: 100,
-              borderColor: "black",
-            }}
-          />
-        </View>
+          {/* username */}
+          <Text style={styles.textTittle}>Username</Text>
+          <Text style={styles.textContainer}>{user.name}</Text>
 
-        {/* username */}
-        <Text style={styles.textTittle}>Username</Text>
-        <Text style={styles.textContainer}>{user.name}</Text>
+          {/* Email */}
+          <Text style={styles.textTittle}>Email</Text>
+          <Text style={styles.textContainer}>{user.email}</Text>
 
-        {/* Email */}
-        <Text style={styles.textTittle}>Email</Text>
-        <Text style={styles.textContainer}>{user.email}</Text>
+          {/* password */}
+          <Text style={styles.textTittle}>Password</Text>
+          <Text style={styles.textContainer}>*************</Text>
 
-        {/* password */}
-        <Text style={styles.textTittle}>Password</Text>
-        <Text style={styles.textContainer}>*************</Text>
-
-        {/* edit button */}
-        <TouchableOpacity style={styles.buttonContainer} onPress={() => {
-          return navigation.navigate("EditProfile")
-        }} >
-          <Text style={{textAlign: 'center'}}>Edit Profile</Text>
-        </TouchableOpacity>
-
-        {/* points group */}
-        <View style={styles.pointsContainer}>
-          
-          <TouchableOpacity style={styles.pointsButtons} onPress={() => {
-            return navigation.navigate("TopUpScreen")
-          }}>
-            <Text style={{textAlign: 'center'}}>Add Points</Text>
+          {/* edit button */}
+          <TouchableOpacity style={styles.buttonContainer} onPress={() => {
+            return navigation.navigate("EditProfile")
+          }} >
+            <Text style={{ textAlign: 'center' }}>Edit Profile</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity style={styles.pointsButtons}>
-            <Text style={{textAlign: 'center'}}>Points: {user.token}</Text>
+          {/* points group */}
+          <View style={styles.pointsContainer}>
+
+            <TouchableOpacity style={styles.pointsButtons} onPress={() => {
+              return navigation.navigate("TopUpScreen")
+            }}>
+              <Text style={{ textAlign: 'center' }}>Add Points</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.pointsButtons}>
+              <Text style={{ textAlign: 'center' }}>Points: {user.token}</Text>
+            </TouchableOpacity>
+          </View>
+
+          <TouchableOpacity style={styles.chatButton} onPress={handleLogout}>
+            <Text style={{ textAlign: 'center' }}>Logout</Text>
           </TouchableOpacity>
+
+
         </View>
-
-        <TouchableOpacity style={styles.chatButton}>
-          <Text style={{textAlign: 'center'}}>Chat</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.chatButton} onPress={handleLogout}>
-          <Text style={{textAlign: 'center'}}>Logout</Text>
-        </TouchableOpacity>
-
-
-      </View>
-    </ScrollView>
-  );
+      </ScrollView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
